3a5f8b642436b0850c2d1be20b2192b02cd230fb
Gabriel <gabriel.jorge@estudiantes.utec.edu.uy>
2023-02-02 20:12:49 -0300

    Day10

--- Day10.py
+++ Day10.py
@@ -76,8 +76,8 @@ try:
         print(logo)
         if not continue_calc:		
             var1 = float(input("Enter a number: "))
-        var2 = float(input("Enter next number: "))
         var3 = input("Enter the operator: ")
+        var2 = float(input("Enter next number: "))
         res = calculate(var1, var2, var3)
         print(f"{var1} {var3} {var2} = {res}")
         aux = input(f"Type 'y' to continue with {res} or 'n' to reset: ")
@@ -90,3 +90,45 @@ except ZeroDivisionError:
         print("No se puede dividir entre 0")
 except Exception as e:
         print(e)
+
+# def add(n1, n2):
+#   return n1 + n2
+
+# def subtract(n1, n2):
+#   return n1 - n2
+
+# def multiply(n1, n2):
+#   return n1 * n2
+
+# def divide(n1, n2):
+#   return n1 / n2
+
+# operations = {
+#   "+": add,
+#   "-": subtract,
+#   "*": multiply,
+#   "/": divide
+# }
+
+# def calculator():
+#   print(logo)
+
+#   num1 = float(input("What's the first number?: "))
+#   for symbol in operations:
+#     print(symbol)
+#   should_continue = True
+ 
+#   while should_continue:
+#     operation_symbol = input("Pick an operation: ")
+#     num2 = float(input("What's the next number?: "))
+#     calculation_function = operations[operation_symbol]
+#     answer = calculation_function(num1, num2)
+#     print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+#     if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+#       num1 = answer
+#     else:
+#       should_continue = False
+#       calculator()
+
+# calculator()
