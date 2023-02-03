4d2669e6c9f04847e8bc692d49c7573babe18103
Gabriel <gabriel.jorge@estudiantes.utec.edu.uy>
2023-02-03 15:21:24 -0300

    Day11

--- Day11.py
+++ Day11.py
@@ -10,60 +10,71 @@
 ## Cards are not removed from the deck as they are drawn.
 ## The computer is the dealer.
 import random
+from Day11_art import logo
+import os
 
-deck = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-u_hand = []
-pc_hand = []
+def game():
+    os.system("clear")
+    print(logo)
 
-continue_game = True
+    deck = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    u_hand = []
+    pc_hand = []
 
-u_hand.append(random.choice(deck))
-u_hand.append(random.choice(deck))
+    continue_game = True
+    another_game = True
 
-pc_hand.append(random.choice(deck))
+    u_hand.append(random.choice(deck))
+    u_hand.append(random.choice(deck))
 
-if sum(u_hand) == 21:
-    print(f"BLACKJACK! You have: {u_hand}")
-    print(f"PC has: {pc_hand} = {sum(pc_hand)}")
-    continue_game = False
+    pc_hand.append(random.choice(deck))
 
-
-while continue_game:
-
-    print(f"You have: {u_hand} = {sum(u_hand)}")
-    print(f"PC has: {pc_hand} = {sum(pc_hand)}")
+    if sum(u_hand) == 21:
+        print(f"\nBLACKJACK! You have: {u_hand} = {sum(u_hand)}")
+        print(f"PC has: {pc_hand} = {sum(pc_hand)}")
+        continue_game = False
 
 
-    opt = input("Type 'y' to get another card or 'n' to pass: ")
-    if opt == "y":
-        u_hand.append(random.choice(deck))
-        if 11 in u_hand and sum(u_hand) > 21:
-                index = u_hand.index(11)
-                u_hand[index] = 1
-        if sum(u_hand) > 21:
-            print(f"You have: {u_hand} = {sum(u_hand)}")
-            print(f"PC has: {pc_hand} = {sum(pc_hand)}")
-            print("YOU LOSE")
-            continue_game = False
-    else:
-        while sum(pc_hand) < 17:
-            pc_hand.append(random.choice(deck))
-            if 11 in pc_hand and sum(pc_hand) > 21:
-                index = pc_hand.index(11)
-                pc_hand[index] = 1
-            if sum(pc_hand) >= 17:
-                if sum(pc_hand) < sum(u_hand) or sum(pc_hand) > 21 and sum(pc_hand) != sum(u_hand):
-                    print(f"You have: {u_hand} = {sum(u_hand)}")
-                    print(f"PC has: {pc_hand} = {sum(pc_hand)}")
-                    print("YOU WIN")
-                    continue_game = False
-                elif sum(pc_hand) > sum(u_hand) and sum(pc_hand) <= 21:
-                    print(f"You have: {u_hand} = {sum(u_hand)}")
-                    print(f"PC has: {pc_hand} = {sum(pc_hand)}")
-                    print("YOU LOSE")
-                    continue_game = False
-                elif sum(pc_hand) == sum(u_hand):
-                    print(f"You have: {u_hand} = {sum(u_hand)}")
-                    print(f"PC has: {pc_hand} = {sum(pc_hand)}")
-                    print("DRAW")
-                    continue_game = False
+    while continue_game:
+
+        print(f"\nYou have: {u_hand} = {sum(u_hand)}")
+        print(f"PC has: {pc_hand} = {sum(pc_hand)}")
+
+
+        opt = input("\nType 'y' to get another card or 'n' to pass: ")
+        if opt == "y":
+            u_hand.append(random.choice(deck))
+            if 11 in u_hand and sum(u_hand) > 21:
+                    index = u_hand.index(11)
+                    u_hand[index] = 1
+            if sum(u_hand) > 21:
+                print(f"\nYou have: {u_hand} = {sum(u_hand)}")
+                print(f"PC has: {pc_hand} = {sum(pc_hand)}")
+                print("YOU LOSE!")
+                continue_game = False
+        else:
+            while sum(pc_hand) < 17:
+                pc_hand.append(random.choice(deck))
+                if 11 in pc_hand and sum(pc_hand) > 21:
+                    index = pc_hand.index(11)
+                    pc_hand[index] = 1
+                if sum(pc_hand) >= 17:
+                    if sum(pc_hand) < sum(u_hand) or sum(pc_hand) > 21 and sum(pc_hand) != sum(u_hand):
+                        print(f"\nYou have: {u_hand} = {sum(u_hand)}")
+                        print(f"PC has: {pc_hand} = {sum(pc_hand)}")
+                        print("YOU WIN")
+                        continue_game = False
+                    elif sum(pc_hand) > sum(u_hand) and sum(pc_hand) <= 21:
+                        print(f"\nYou have: {u_hand} = {sum(u_hand)}")
+                        print(f"PC has: {pc_hand} = {sum(pc_hand)}")
+                        print("YOU LOSE")
+                        continue_game = False
+                    elif sum(pc_hand) == sum(u_hand):
+                        print(f"\nYou have: {u_hand} = {sum(u_hand)}")
+                        print(f"PC has: {pc_hand} = {sum(pc_hand)}")
+                        print("IT'S A DRAW")
+                        continue_game = False
+    if input("\nPlay another hand? : 'y' or 'n': ") == "y":
+        game()
+
+game()
